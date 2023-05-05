c977db5997750919557127e051b35bf1599a89f7
Abde <agb139@alu.ua.es>
2023-05-05 15:50:22 +0200

    repgrid beta

---
+++ Servers/repgrid_analysis_server.R
@@ -0,0 +1,101 @@
+repgrid_analisis_server <- function(input, output, session) {
+
+  repgrid_data <- OpenRepGrid::importExcel("Servers/Repgrid_data.xlsx")
+  indices_list <- gridindices(boeker)
+  # Generar gráfico bidimensional
+  output$biplot2d_plot <- renderPlot({
+   
+    OpenRepGrid::biplot2d(repgrid_data, c.label.col = "#005440")
+  })
+
+  # Generar gráfico tridimensional
+  output$biplot3d_plot <- renderRglwidget({
+   
+    #OpenRepGrid::biplot3d(repgrid_data)
+    
+    try(close3d())
+    #points3d(1:10, 1:10, 1:10)
+    #axes3d()
+    OpenRepGrid::biplot3d(boeker, c.label.col = "#005440")
+    rglwidget()
+    #try(close3d())
+    #OpenRepGrid::biplot3d(repgrid_data)
+    #points3d(1:10, 1:10, 1:10)
+    #axes3d()
+    #rglwidget()
+  }, outputArgs = list(width = "auto", height = "300px"))
+
+  # Generar análisis por conglomerados
+  output$cluster_plot_1 <- renderPlot({
+    
+    OpenRepGrid::cluster(repgrid_data,along=1)
+  })
+
+  # Generar análisis por conglomerados
+  output$cluster_plot_2 <- renderPlot({
+    
+    OpenRepGrid::cluster(repgrid_data,along=2)
+  })
+
+
+  # Generar tabla de índices y valores matemáticos
+  output$gridindices_table <- renderText({
+    
+    PVEFF <- indices_list[["pvaff"]] 
+    INT <- indices_list[["intensity"]][["Total"]] 
+    CON <- indices_list[["conflict"]]
+    BIA <- indices_list[["bias"]]
+    knitr::kable(data.frame(PVEFF,INT,CON,BIA),col.names = c("PVAFF","Intensity","Conflicts","BIAS"),format = "html") %>%
+    kable_styling("striped", full_width = T) %>%
+    row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    column_spec(1, bold = T, color = "#005440", width = "25%") %>%
+    column_spec(2, width = "25%") %>%
+    column_spec(3, width = "25%") %>%
+    column_spec(4, width = "25%")
+  })
+  
+  output$construct <- renderText({
+    
+    INTc <- indices_list[["intensity"]][["Constructs"]] 
+
+    knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
+    kable_styling("striped", full_width = F) %>%
+    row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    column_spec(1, bold = T, color = "#005440")
+  })
+
+
+ output$elementss <- renderText({
+    
+    INTe <- indices_list[["intensity"]][["Elements"]] 
+    knitr::kable(INTe, col.names = "Intensity",format = "html") %>%
+    kable_styling("striped", full_width = F) %>%
+    row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    column_spec(1, bold = T, color = "#005440")
+  })
+
+
+  output$constructs <- renderText({
+    
+    INTc <- indices_list[["dilemmas"]][["congruency"]] 
+
+    knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
+    kable_styling("striped", full_width = F) %>%
+    row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    column_spec(1, bold = T, color = "#005440")
+  })
+
+
+  output$dilemmasss <- renderText({
+    
+    INTc <- indices_list[["dilemmas"]][["dilemmas"]] 
+
+    knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
+    kable_styling("striped", full_width = F) %>%
+    row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    column_spec(1, bold = T, color = "#005440")
+  })
+
+  
+
+}
--- Servers/repgrid_home_servers.R
+++ Servers/repgrid_home_servers.R
@@ -1,30 +1,56 @@
-library(OpenRepGrid)
-
 repgrid_home_server <- function(input, output, session) {
   # Mostrar los datos importados en una tabla
-  # output$tabla_datos_repgrid <- renderTable({#session$userData$datos_repgrid() # a <- OpenRepGrid::importExcel("Servers/Repgrid_data.xlsx")a})
-  datos <- OpenRepGrid::importExcel("Servers/Repgrid_data.xlsx")
-  #datos <- datos$data
-  datos <- NULL
-
+  datos_initial <- session$userData$datos_repgrid
+  datos1 <- reactiveVal(datos_initial)
+  datos <- reactiveVal(session$userData$datos_to_table)
 
   output$tabla_datos_repgrid <- DT::renderDataTable({
-    DT::datatable(datos)
+    DT::datatable(datos(), 
+                  class = 'my-custom-table', 
+                  options = list(autoWidth = TRUE, columnDefs = list(list(width = '30px', targets = "_all"))), editable = TRUE)
   })
 
-
-  # Agregar observadores para los botones de análisis
-  observeEvent(input$analisis1, {
-    # Código para realizar el análisis 1
-    print("analisis1: ")
-    print(session$userData$datos_repgrid)
+  output$prueba <- renderPlot({
+    bertin(datos1(), color=c("white", "#005440"))
   })
 
-  observeEvent(input$analisis2, {
-    # Código para realizar el análisis 2
+  # Observar cambios en la tabla editable y actualizar los datos de la sesión
+  observeEvent(input$tabla_datos_repgrid_cell_edit, {
+    # Obtener la información de la celda editada
+    info <- input$tabla_datos_repgrid_cell_edit
+    row <- info$row
+    col <- info$col
+    value <- info$value
+
+    # Actualizar el valor en el objeto data.frame
+    datos_temp <- datos()
+    datos_temp[row, col] <- value
+    datos(datos_temp)
+
   })
 
-  observeEvent(input$analisis3, {
-    # Código para realizar el análisis 3
+  observeEvent(input$editar, {
+    # Ocultar el botón "Editar" y mostrar el botón "Guardar"
+    shinyjs::hide("editar")
+    shinyjs::show("guardar")
+    # Cambiar a modo de edición
+    shinyjs::hide("prueba_container")
+    shinyjs::show("tabla_datos_repgrid_container")
+  })
+
+  observeEvent(input$guardar, {
+    # Ocultar el botón "Guardar" y mostrar el botón "Editar"
+    shinyjs::hide("guardar")
+    shinyjs::show("editar")
+    # Cambiar a modo de visualización
+    shinyjs::hide("tabla_datos_repgrid_container")
+    shinyjs::show("prueba_container")
+    
+    # Convertir el data.frame a repgrid
+    #datos_df <- datos()
+    #constructs <- datos_df %>% dplyr::select(-1)
+    #elements <- datos_df[[1]]
+    #datos1_temp <- OpenRepGrid::constructRepgrid(elements, constructs)
+    #datos1(datos1_temp)
   })
 }
