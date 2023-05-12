86838abd1b030c009ffee06fe663c3b4f77873f8
Abde <agb139@alu.ua.es>
2023-05-12 10:38:16 +0200

    solv dilemma

--- Servers/repgrid_analysis_server.R
+++ Servers/repgrid_analysis_server.R
@@ -3,6 +3,9 @@ repgrid_analisis_server <- function(input, output, session) {
   repgrid_data <- OpenRepGrid::importExcel("Servers/Repgrid_data.xlsx")
   indices_list <- gridindices(repgrid_data)
   # Generar gráfico bidimensional
+  print(repgrid_data)
+  #print(indices_list)
+
   output$biplot2d_plot <- renderPlot({
     
     OpenRepGrid::biplot2d(repgrid_data, c.label.col = "#005440",c.grid = "gray", c.grid.lty = "dotted", c.grid.lwd = 0.5, cex.axis = 0.8, cex.labels = 0.8,)
@@ -16,7 +19,7 @@ repgrid_analisis_server <- function(input, output, session) {
     try(close3d())
     #points3d(1:10, 1:10, 1:10)
     #axes3d()
-    OpenRepGrid::biplot3d(boeker, c.label.col = "#005440")
+    OpenRepGrid::biplot3d(repgrid_data)
     rglwidget()
     #try(close3d())
     #OpenRepGrid::biplot3d(repgrid_data)
@@ -28,13 +31,15 @@ repgrid_analisis_server <- function(input, output, session) {
   # Generar análisis por conglomerados
   output$cluster_plot_1 <- renderPlot({
     
-    OpenRepGrid::cluster(repgrid_data,along=1)
+    #OpenRepGrid::cluster(repgrid_data,along=1)
+     indices_list[["distances"]][["Constructs"]] 
   })
 
   # Generar análisis por conglomerados
   output$cluster_plot_2 <- renderPlot({
     
-    OpenRepGrid::cluster(repgrid_data,along=2)
+    #OpenRepGrid::cluster(repgrid_data,along=2)
+    indices_list[["distances"]][["Elements"]] 
   })
 
 
@@ -77,23 +82,25 @@ repgrid_analisis_server <- function(input, output, session) {
 
   output$constructs <- renderText({
     
-    INTc <- indices_list[["dilemmas"]][["Constructs"]] #Constructs congruency
-
-    knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
-    kable_styling("striped", full_width = F) %>%
-    row_spec(0, bold = T, color = "white", background = "#005440") %>%
-    column_spec(1, bold = T, color = "#005440")
+    INTc <- indices_list[["dilemmas"]][["Congruency"]] #Constructs congruency
+    #print(indexDilemma(repgrid_data)[[1]])
+    knitr::kable(INTc,format = "html") %>%
+    #knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
+    kable_styling("striped", full_width = F) #%>%
+    #row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    #column_spec(1, bold = T, color = "#005440")
   })
 
 
   output$dilemmasss <- renderText({
     
-    INTc <- indices_list[["dilemmas"]][["Elements"]] #dilemmas
-
-    knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
-    kable_styling("striped", full_width = F) %>%
-    row_spec(0, bold = T, color = "white", background = "#005440") %>%
-    column_spec(1, bold = T, color = "#005440")
+    INTc <- indices_list[["dilemmas"]][["Dilemmas"]] #dilemmas
+    print(INTc)
+    knitr::kable(INTc,format = "html") %>%
+    #knitr::kable(INTc, col.names = "Intensity",format = "html") %>%
+    kable_styling("striped", full_width = F) #%>%
+    #row_spec(0, bold = T, color = "white", background = "#005440") %>%
+    #column_spec(1, bold = T, color = "#005440")
   })
 
   
