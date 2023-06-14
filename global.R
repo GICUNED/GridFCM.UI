cd6249efde8d8b5a7c386dde7a53460149addf58
Abde <agb139@alu.ua.es>
2023-06-14 15:48:02 +0200

    Json format 3.

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
 
 
-i18n <- Translator$new(translation_json_path = "Traductions/ALL.json")
+i18n <- Translator$new(translation_json_path = "Traductions/wimpgrid.json")
 i18n$set_translation_language("ENG")
