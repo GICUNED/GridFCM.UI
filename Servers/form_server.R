8f298ac028a3a89882dc846e4c35fa57ecae9dc7
Simon <simonpedro.kaufmann@stratebi.com>
2023-09-27 14:00:51 +0200

    Avanzando en formularios

--- Servers/form_server.R
+++ Servers/form_server.R
@@ -1,27 +1,48 @@
 form_server <- function(input, output, session){
-    shinyjs::hide("namesForm")
-    nombres <- reactiveVal(character(0))
-    lista_nombres <- list()
-    nombre_seleccionado <- reactiveVal(NULL)
+    shinyjs::hide("listadoElementos")
+    shinyjs::hide("Constructos")
+    shinyjs::hide("preguntasDiadas")
+    shinyjs::hide("ConstructosAleatorios")
+    shinyjs::hide("n_aleatorio")
 
-    # Botón añadir 
+    nombres <- reactiveVal(list("Yo - Actual", "Yo - Ideal"))
+    nombre_seleccionado <- reactiveVal(NULL)
+    constructos <- reactiveVal(NULL)
+    constructo_seleccionado <- reactiveVal(NULL)
+    aleatorios <- reactiveVal(NULL)
+
+    # Formulario para elementos
+
     observeEvent(input$guardarNombre, {
-        if (nchar(input$nombrePaciente) > 0) {
-            shinyjs::show("namesForm")
-            nombres(c(nombres(), input$nombrePaciente))
+        if (nchar(input$nombrePaciente) > 2) {
+            nombres <- c(nombres(), as.character(input$nombrePaciente))
+            shinyjs::show("listadoElementos")
+            if("Yo - Ideal" %in% nombres){
+                posicion_ideal <- which(nombres == "Yo - Ideal")
+            }else{
+                posicion_ideal <- -1
+            }
+            # Mueve "Yo - Ideal" al final de la lista
+            if(posicion_ideal != -1){
+                nombres <- c(nombres[-posicion_ideal], nombres[posicion_ideal])
+            }
+            nombres(nombres)
             #reactiveVal entre parentesis sin parametros devuelve el valor del objeto
             
             updateTextInput(session, "nombrePaciente", value = "")
+
+            output$lista_nombres <- renderUI({
+                if (length(nombres) > 0) {
+                    menu_items <- lapply(nombres, function(nombre) {
+                        menuItem(nombre, icon = icon("user"), tabName=nombre)
+                    })
+                    sidebarMenu(id="menu_elementos", menu_items)
+                } 
+            })
         }
-        output$lista_nombres <- renderUI({
-            if (length(nombres()) > 0) {
-                menu_items <- lapply(nombres(), function(nombre) {
-                    menuItem(nombre, icon = icon("user"), tabName=nombre)
-                })
-                sidebarMenu(id="menu_elementos", menu_items)
-            } 
-        })
+        
     })
+
     observe(
         if(!is.null(input$menu_elementos)){
             nombre_seleccionado(input$menu_elementos)
@@ -48,12 +69,146 @@ form_server <- function(input, output, session){
         }
     })
 
-    
-    observeEvent(input$continuar, {
-        # cambiar luego esto para que se vaya construyendo el dataframe para luego meterlo en la bd y 
-        # posteriormente pasarle el codificar y decodificar....
-        lista_nombres <- list(nombres())
-        session$userData$repgrid_form$elementos = lista_nombres
-        message(session$userData$repgrid_form$elementos)
+    observeEvent(input$continuar_elementos, {
+        lista_nombres <- nombres()
+        # Asigna la lista reordenada a la variable reactiva nombres()
+        session$userData$repgrid_form$elementos = list(lista_nombres)
+
+        shinyjs::hide("Elementos")
+        shinyjs::show("preguntasDiadas")
+    })    
+
+    # Preguntas sobre los constructos
+    observeEvent(input$aleatorio, {
+        shinyjs::show("n_aleatorio")
+        shinyjs::show("generar_aleatorio")
+    })
+
+    observeEvent(input$manual, {
+        shinyjs::show("Constructos")
+        shinyjs::hide("preguntasDiadas")
+    })
+
+    observeEvent(input$generar_aleatorio, {
+        generar_diadas(input$n_aleatorio)
+    })
+
+    observeEvent(input$atras_preguntas_diada, {
+        shinyjs::hide("preguntasDiadas")
+        shinyjs::show("Elementos")
+    })
+
+
+    # Formulario para constructos manuales
+
+    observe(
+        if((input$constructo_izq != "") && (input$constructo_der != "")){
+            shinyjs::enable("guardarConstructo")
+        }
+        else{
+            shinyjs::disable("guardarConstructo")
+        }
+    )
+
+    observeEvent(input$guardarConstructo, {
+        if((length(input$constructo_izq) > 0) && (length(input$constructo_der) > 0)){
+            constructo <- paste(input$constructo_izq, " - ", input$constructo_der)
+            constructos(c(constructos(), constructo))
+            updateTextInput(session, "constructo_izq", value="")
+            updateTextInput(session, "constructo_der", value="")
+            output$lista_constructos <- renderUI({
+                menu_items <- lapply(constructos(), function(nombre) {
+                    menuItem(nombre, icon = icon("user"), tabName=nombre)
+                })
+                sidebarMenu(id="menu_constructos", menu_items)
+            
+            })
+        }
+    })
+
+    observe(
+        if(!is.null(input$menu_constructos)){
+            constructo_seleccionado(input$menu_constructos)
+            shinyjs::enable("borrarConstructo")
+        }
+        else{
+            shinyjs::disable("borrarConstructo")
+        }
+    )
+
+    observeEvent(input$borrarConstructo, {
+        constructo <- constructo_seleccionado()
+        if(!is.null(constructo)){
+            constructos <- constructos()
+            # Eliminar el nombre seleccionado
+            lista_constructos <- constructos[constructos != constructo]
+            constructos(lista_constructos)
+            output$lista_constructos <- renderUI({
+                menu_items <- lapply(constructos(), function(nombre) {
+                    menuItem(nombre, tabName=nombre)
+                })
+                sidebarMenu(id="menu_constructos", menu_items)
+            })
+        }
+    })
+
+    observeEvent(input$continuar_constructo, {
+        constructos <- constructos()
+        # Asigna la lista reordenada a la variable reactiva nombres()
+        session$userData$repgrid_form$constructos = list(constructos)
+        message(session$userData$repgrid_form$constructos)
+
+        shinyjs::hide("Constructos")
+    })  
+
+    observeEvent(input$atras_constructos, {
+        shinyjs::hide("Constructos")
+        shinyjs::show("preguntasDiadas")
+    })
+
+    # Formulario para constructos aleatorios
+
+    generar_diadas <- function(n_pares){
+        # elementos sin el yo-ideal que debería estar último
+        yo_actual <- nombres()[1]
+        elementos <- nombres()
+        without_yo_ideal <- (length(elementos)-1)
+        elementos <- elementos[2 : without_yo_ideal]
+        if(n_pares > length(elementos) || n_pares < 1){
+            showModal(modalDialog(
+                title = paste(i18n$t("Debe introducir un número entre 1 y "), length(elementos)),
+                footer = tagList(
+                    modalButton("OK"),
+                )
+            ))
+        }
+        else{
+            pares_seleccionados <- list()
+            elementos_aleatorios <- list()
+            # Realiza la selección n_pares veces
+                # Selecciona dos valores aleatorios sin reemplazo
+            aleatorio <- sample(elementos, size = n_pares, replace = FALSE)
+            for(i in 1:n_pares){
+                pareja <- c(yo_actual, aleatorio[i])
+                elementos_aleatorios <- c(elementos_aleatorios, pareja)
+            }
+
+            aleatorios(elementos_aleatorios)
+            message(aleatorios())
+            shinyjs::show("ConstructosAleatorios")
+            shinyjs::hide("preguntasDiadas")
+        }
+    }
+
+    observe(
+        output$pregunta_semejanza <- renderText({
+
+            paste("En qué se parecen tu YO ACTUAL y tu ", aleatorios()[2])
+        })
+    )
+
+    observeEvent(input$atras_constructos_aleatorios, {
+        shinyjs::hide("ConstructosAleatorios")
+        shinyjs::show("preguntasDiadas")
     })
 }
--- UI/form_ui.R
+++ UI/form_ui.R
@@ -4,14 +4,14 @@ form_ui <- fluidPage( class="header-tab mix-diff",
 
 tabsetPanel(
     
-  tabPanel(i18n$t("RepGrid"), id = "tab_data_w", icon = icon("magnifying-glass-chart"),
+    tabPanel(i18n$t("RepGrid"), id = "tab_data", icon = icon("magnifying-glass-chart"),
 
-    fluidRow(class = ("flex-container-titles"),
-        h2(i18n$t("Inicio formulario RepGrid"), class = "rg pagetitlecustom mt-2"),
-    ),
-    
-    fluidRow(class = "mt-4 custom-margins justify-content-center align-items-start",
-            column(5,
+        fluidRow(class = ("flex-container-titles"),
+            h2(i18n$t("Inicio formulario RepGrid"), class = "rg pagetitlecustom mt-2"),
+        ),
+        
+        fluidRow(id="Elementos", class = "mt-4 custom-margins justify-content-center align-items-start",
+            column(5, id= "formElementos",
                 box(
                     width = 12,
                     title = i18n$t("Elementos a valorar"),
@@ -22,32 +22,78 @@ tabsetPanel(
                     column(12, class="d-flex justify-content-center mt-3", actionButton("guardarNombre", i18n$t("Añadir"), status = "primary", icon = icon("plus")))
                 )
             ),
+            column(7, id = "listadoElementos",
+                box(
+                        width = 12,
+                        title = i18n$t("Nombres Guardados"),
+                        icon = icon("person"),
+                        status = "success",
+                        collapsible = FALSE,
+                        actionButton("borrarElemento", i18n$t("Borrar"), status ="danger", disabled=TRUE, icon = icon("trash-can")),
+                        
 
-            
-            column(7, id = "namesForm",
+                        column(12, shinycssloaders::withSpinner(uiOutput("lista_nombres"), type = 4, color = "#022a0c", size = 0.6)),
+                        column(12, class="d-flex justify-content-center mt-3", actionButton("continuar_elementos", i18n$t("Continuar"), status="success", icon = icon("arrow-right") ))
+                ),
+            ),
+        ),
+
+        column(12, id = "preguntasDiadas",
             box(
-                    width = 12,
-                    title = i18n$t("Nombres Guardados"),
-                    icon = icon("person"),
-                    status = "success",
-                    collapsible = FALSE,
-                    actionButton("borrarElemento", i18n$t("Borrar"), status ="danger", disabled=TRUE, icon = icon("trash-can")),
-                    
-
-                    column(12, shinycssloaders::withSpinner(uiOutput("lista_nombres"), type = 4, color = "#022a0c", size = 0.6)),
-                    column(12, class="d-flex justify-content-center mt-3", actionButton("continuar", i18n$t("Continuar"), status="success", icon = icon("arrow-right") ))
+                title = i18n$t("¿Desea añdir los constructos de forma manual o de forma aleatoria?"),
+                actionButton("manual", i18n$t("Manual")),
+                actionButton("aleatorio", i18n$t("Aleatoria")),
+                numericInput("n_aleatorio", i18n$t("Número de constructos que desea generar:"), value = 1, step=1),
+                actionButton("generar_aleatorio", i18n$t("Generar diadas"), style = "display: none;"),
+                actionButton("atras_preguntas_diada", i18n$t("Atrás"))
             )
         ),
-    
-    )
-),
+        
+        fluidRow(id="Constructos", class = "mt-4 custom-margins justify-content-center align-items-start",
+            
+            textInput("constructo_izq", i18n$t("Polo izquierdo:"), ""),
+            
+            textInput("constructo_der", i18n$t("Polo derecho:"), ""),
+            
+            # boton add constructos
+            actionButton("guardarConstructo", i18n$t("Añadir"), disabled=TRUE),
 
-tabPanel(i18n$t("WimpGrid"), id = "tab_data_w", icon = icon("border-none"),
+            column(7, id = "listadoConstructos",
+                box(
+                        width = 12,
+                        title = i18n$t("Constructos guardados"),
+                        status = "success",
+                        collapsible = FALSE,
+                        actionButton("borrarConstructo", i18n$t("Borrar"), status ="danger", disabled=TRUE, icon = icon("trash-can")),
+                        
+
+                        column(12, shinycssloaders::withSpinner(uiOutput("lista_constructos"), type = 4, color = "#022a0c", size = 0.6)),
+                        column(12, class="d-flex justify-content-center mt-3", actionButton("continuar_constructo", i18n$t("Continuar"), status="success", icon = icon("arrow-right") )),
+                        actionButton("atras_constructos", i18n$t("Atrás"))
+                ),
+            ),
+        ),
+        fluidRow(id="ConstructosAleatorios", class = "mt-4 custom-margins justify-content-center align-items-start",
+            box(
+                textOutput("pregunta_semejanza"),
+                textInput("respuesta_semejanza_1", i18n$t("Ambos somos:")),
+                textInput("respuesta_semejanza_2", i18n$t("¿Qué sería en, tu opinión, lo opuesto?")),
+                textOutput("pregunta_diferencia"),
+                textInput("respuesta_diferencia_1", i18n$t("Yo soy:")),
+                textOutput("pregunta_diferencia_2"),
+                textInput("respuesta_diferencia_2", ""),
+                actionButton("atras_constructos_aleatorios", i18n$t("Atrás"))
+            ),
+        ),
+        
+    ),
+    tabPanel(i18n$t("WimpGrid"), id = "tab_data_w", icon = icon("border-none"),
 
         fluidRow(class = ("flex-container-titles"),
             h2(i18n$t("Inicio formulario WimpGrid"), class = "wg pagetitlecustom mt-4"),
         ),
     
-    )),
+    )
+),
 
 )
