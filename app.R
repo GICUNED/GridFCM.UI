da3d0651b0bbb22f5cdb0708ee4b95b9774be367
Ivan Lopez Ripoll <ivan.lopez@stratebi.com>
2023-11-29 13:56:13 +0100

    arreglado is valid email not found

--- app.R
+++ app.R
@@ -471,6 +471,10 @@ observeEvent(input$invitado, {
       ))
   })
 
+  isValidEmail <- function(x) {
+    grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x), ignore.case=TRUE)
+  }
+  
   observeEvent(input$entrar_invitado, {
     email <- input$email_invitado
     if(!isValidEmail(email)){
